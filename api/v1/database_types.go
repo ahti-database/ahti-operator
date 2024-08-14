cba997f03ec351ae6cc9a6544b249722e930609d
Jason Cheng <jasoncky96@gmail.com>
2024-08-14 16:31:53 +0800

    remove lastSyncTime

--- api/v1/database_types.go
+++ api/v1/database_types.go
@@ -51,7 +51,6 @@ type DatabaseSpec struct {
 type DatabaseStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
-	LastSyncTime metav1.Time `json:"lastSyncTime"`
 }
 
 //+kubebuilder:object:root=true
